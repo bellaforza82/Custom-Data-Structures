61466e1c71994611e5ccc41796ede80fc79f8864
Neil Allen Jones <neilallenjones@gmail.com>
2017-05-30 13:28:28 -0400

    Add files via upload

---
+++ Queue.java
@@ -0,0 +1,50 @@
+/**
+ * Created by horry on 5/30/2017.
+ */
+import java.util.Iterator;
+public class Queue<Item> implements Iterable<Item> {
+    private Node first; //link to least recently added node
+    private Node last;  //last Node added
+    private int N;
+
+    private class Node{
+        Item item;
+        Node next;
+    }
+    public boolean isEmpty() {return first == null;}
+    public int size()        {return N;}
+    public void enqueue(Item item){
+        //Add item to end of list
+        Node oldlast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) first = last;
+        else           oldlast.next = last;
+        N++;
+    }
+    public Item dequeue(){
+        //Remove item from beginning of list
+        Item item = first.item;
+        first = first.next;
+        N--;
+        if (isEmpty()) last = null;
+        return item;
+    }
+    public Iterator<Item> iterator(){
+        return new ListIterator();
+    }
+    private class ListIterator implements Iterator<Item>{
+        private Node current = first;
+        public boolean hasNext(){
+            return current != null;
+        }
+        public void remove(){}
+        public Item next(){
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+}
